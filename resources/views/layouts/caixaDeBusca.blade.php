63c56104b6324b83c69500bc07317c4ae2793a06
Lucas Schw <10210338+lucasschw@users.noreply.github.com>
2022-03-02 14:30:16 -0300

    Ajusta a action da caixa de busca

--- resources/views/layouts/caixaDeBusca.blade.php
+++ resources/views/layouts/caixaDeBusca.blade.php
@@ -1,5 +1,5 @@
 
-<form id="buscaRecursosTA" method="get" action="/filtro">
+<form id="buscaRecursosTA" method="get" action="{{url('/filtro')}}">
 	<div class="row col-sm-12 col-12 justify-content-center justify-content-sm-center no-gutters input-group mb-3">
 		<div class="col-sm-9 col-7">
 			<input type="text" name="texto" class="form-control" placeholder="Busque recursos de tecnologia assistiva" aria-label="Campo de busca com seletor para optar entre buscar por TAGs ou termos" required>
