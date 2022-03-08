00ce23b531b889d4329fd115261b80b3eb1e0f1c
Lucas Schw <10210338+lucasschw@users.noreply.github.com>
2022-03-08 09:24:51 -0300

    Ajusta o layout em 'aprender', 'sobre' e na caixa de autocompletar da busca

--- resources/views/aprender.blade.php
+++ resources/views/aprender.blade.php
@@ -18,19 +18,22 @@
 
 <div class="container card my-3">
 	<div class="row">
-        <div class="col-12">
-            <div class="row">
-                <h2 class="display-3 pl-4 h1">Aprender</h2>
+        @if ($conteudoPagina->texto == '')
+            <div class="col-12">
+                <div class="row">
+                    <h2 class="display-3 pl-4 h1">Aprender</h2>
+                </div>
+                <div class="row">
+                    <p class="lead pl-4">Acesse materiais para estudar mais sobre Tecnologias Assistivas</p> 
+                </div>
             </div>
-            <div class="row">
-                <p class="lead pl-4">Acesse materiais para estudar mais sobre Tecnologias Assistivas</p> 
-            </div>
-        </div>
-		<div id="conteudo" class="col-12 pt-5 pl-5" >
-			<h3 class="display-4 h1">
+        @endif
+
+		<div id="conteudo" class="col-12 pt-2 pl-2" >
+			<h3 class="display-4 h1 text-center mt-3 mb-5 ml-5 mr-5">
 				{!! html_entity_decode(stripslashes($conteudoPagina->titulo_texto), ENT_QUOTES, 'UTF-8')!!} 
 			</h3>
-			<div class="my-3 ml-5">
+			<div class="my-3 ml-5 mr-5">
 				{!! html_entity_decode(stripslashes($conteudoPagina->texto), ENT_QUOTES, 'UTF-8')!!}			
 			</div>		
 		</div>
