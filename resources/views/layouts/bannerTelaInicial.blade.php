4750fd1fde7e0484e24355eca7ca8bcf335800f6
Luana Chiomento <luanachiomento@gmail.com>
2025-02-21 09:11:19 -0300

    primeiras alterações

--- resources/views/layouts/bannerTelaInicial.blade.php
+++ resources/views/layouts/bannerTelaInicial.blade.php
@@ -1,5 +1,5 @@
 <div class="container h-50  mw-100 banner pb-5">
-	<div class="row pb-5">
+	<div class="col pb-5">
 		<div class="col-md-5">
 			<div id="card-retace-intro" class="card mt-5 h-100">
 				<div class="card-body h-100">
@@ -8,12 +8,6 @@
 							<h2 class="h1 text-center mb-5">
 								Repositório de Tecnologia Assistiva no Contexto Educacional
 							</h2>
-							<p class="pl-5 pr-5 text-justify">
-								Esse repositório tem como objetivo reunir recursos de tecnologia assistiva
-								que podem ser utilizados no contexto educacional assim como materiais 
-								pedagógicos acessíveis, informações, manuais e instruções de como 
-								reproduzir esses recursos.
-							</p>
 						</div>
 					</div>
 				</div>
