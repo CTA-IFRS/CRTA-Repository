7fd64db0307f712f4d019a8893221944ff64e333
Luana Chiomento <luanachiomento@gmail.com>
2025-03-04 13:31:12 -0300

    atualização das páginas acessibilidade e sobre

--- resources/views/acessibilidade.blade.php
+++ resources/views/acessibilidade.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.siteLayout')
 @section('titulo','RETACE - Acessibilidade')
 @section('conteudo')
-<div class="container mt-5">
+<div class="container container-xl mt-5">
 	<?php 
         $data = [
             ['name' => 'RETACE', 'link' => url('/')],
@@ -13,17 +13,18 @@
             @include('layouts.breadcrumb', $data)
         </div>
     </div>
+    <hr class="mt-0">
 </div>
 
-<div class="container card my-3">
+<div id="acessibilidade" class="custom_content container custom-container card my-3 bg-transparent">
 	<div class="row">
         <div class="col-12">
             <div class="row">
-                <h2 class="display-3 pl-4 h1">Acessibilidade</h2>
+                <h2 class="h1 m-auto pt-4">Acessibilidade</h2>
             </div>
         </div>
-		<div id="conteudo" class="col-12 pt-5 pl-5" >
-			<h3>Acessibilidade de nosso site</h3>
+		<div id="conteudo" class="col-12 pt-4">
+			<h3 class="mb-3">Acessibilidade de nosso site</h3>
             <p>De acordo com o&nbsp;<a class="link-primary" href="http://www.w3c.br/">W3C&nbsp;(World Wide Web Consortium)</a>, 
                 Acessibilidade na Web significa garantir que todas as pessoas, incluindo pessoas com 
                 defici&ecirc;ncia, possam utilizar a Web. Mais especificamente, significa permitir que 
@@ -38,23 +39,52 @@
                  e <a class="link-primary" href="http://emag.governoeletronico.gov.br/">eMAG 3.1&nbsp;(Modelo de Acessibilidade em Governo Eletr&ocirc;nico &ndash; nacional)</a>.
             </p>
             
-            <p>No topo das p&aacute;ginas de nosso site, disponibilizamos uma barra de acessibilidade, que cont&eacute;m:</p>
-            <ul>
-                <li><strong>Atalhos de teclado:</strong>&nbsp;permitem ir diretamente a um bloco do site, 
-                    facilitando a navega&ccedil;&atilde;o para quem utiliza o teclado, como pessoas cegas e 
-                    com certas limita&ccedil;&otilde;es f&iacute;sicas;
+            <p>
+                <b>No topo das p&aacute;ginas de nosso site, disponibilizamos uma barra de acessibilidade, que cont&eacute;m:</b>
+            </p>
+            
+            <ul class="d-flex flex-column p-0 pt-2">
+                <li class="bg-white d-flex rounded-lg">
+                    <div class="row w-100 m-0 py-4 px-3">
+                        <div class="col-12 d-flex flex-column">
+                            <strong class="mb-2">Atalhos de teclado</strong>
+                            Permitem ir diretamente a um bloco do site, 
+                            facilitando a navega&ccedil;&atilde;o para quem utiliza o teclado, como pessoas cegas e 
+                            com certas limita&ccedil;&otilde;es f&iacute;sicas.
+                        </div>
+                    </div>
                 </li>
-                <li><strong>P&aacute;gina de acessibilidade:</strong>&nbsp;apresenta informa&ccedil;&otilde;es sobre 
-                    a acessibilidade do site, recursos oferecidos e testes realizados;
+                <li class="bg-white d-flex rounded-lg">
+                    <div class="row w-100 m-0 py-4 px-3">
+                        <div class="col-7 d-flex flex-column">
+                            <strong class="mb-2">P&aacute;gina de acessibilidade</strong>
+                            Apresenta informa&ccedil;&otilde;es sobre 
+                            a acessibilidade do site, recursos oferecidos e testes realizados;
+                        </div>
+                        <div class="col-5">
+                        <img src="{{url('/imagens/pag_acessibilidade.png')}}" alt="imagem mostrando a página de acessibilidade" class="img-fluid rounded">
+                        </div>
+                    </div>
                 </li>
-                <li><strong>Mapa do site:</strong>&nbsp;disponibiliza todas as p&aacute;ginas do site de 
-                    forma hier&aacute;rquica, permitindo que o usu&aacute;rio conhe&ccedil;a toda a estrutura 
-                    do site e acesse diretamente a p&aacute;gina desejada.
+                <li class="bg-white d-flex rounded-lg">
+                    <div class="row w-100 m-0 py-4 px-3">
+                        <div class="col-7 d-flex flex-column">
+                            <strong class="mb-2">Mapa do site</strong>
+                            Disponibiliza todas as p&aacute;ginas do site de 
+                            forma hier&aacute;rquica, permitindo que o usu&aacute;rio conhe&ccedil;a toda a estrutura 
+                            do site e acesse diretamente a p&aacute;gina desejada.
+                        </div>
+                        <div class="col-5">
+                            <img src="{{url('/imagens/mapa_site.png')}}" alt="imagem mostrando o menu com o mapa do site" class="img-fluid rounded">
+                        </div>
+                    </div>
                 </li>
             </ul>
 
-            <h3>Utilizando os atalhos</h3>
-            <p>Em nosso site, disponibilizamos quatro atalhos de teclado:</p>
+            <h3 class="mt-5 mb-3">Utilizando os atalhos</h3>
+            <p>
+                <b>Em nosso site, disponibilizamos quatro atalhos de teclado:</b>
+            </p>
             <ul>
                 <li>1: Ir para o conte&uacute;do</li>
                 <li>2: Ir para o menu</li>
@@ -62,7 +92,9 @@
                 <li>4: Ir para rodap&eacute;</li>
             </ul>
 
-            <p>Cada navegador possui tecla ou teclas espec&iacute;ficas para ativar os atalhos:</p>
+            <p>
+                <b>Cada navegador possui tecla ou teclas espec&iacute;ficas para ativar os atalhos:</b>
+            </p>
             <ul>
                 <li>Chrome:&nbsp;<strong>Alt + [n&uacute;mero do atalho]</strong>&nbsp;(Windows/Linux) 
                     ou&nbsp;<strong>Control + Option + [n&uacute;mero do atalho]</strong>&nbsp;(Mac)
@@ -78,7 +110,7 @@
                 </li>
             </ul>
             
-            <h3>Ampliando o texto no navegador</h3>
+            <h3 class="mt-5 mb-3">Ampliando o texto no navegador</h3>
             <ul>
                 <li>Pressione&nbsp;<strong>&ldquo;Ctrl&rdquo;</strong>&nbsp;+&nbsp;<strong>&ldquo;+&rdquo;</strong>&nbsp;
                     (sinal de mais) para aumentar a fonte do texto;
@@ -92,23 +124,31 @@
             </ul>
             
             <p>No Mac OS, substitua o &ldquo;Ctrl&rsquo; pela tecla &ldquo;Command&rdquo;.</p>
+            <br>
 
-            <h3>Avalia&ccedil;&atilde;o de acessibilidade</h3>
-            <p>&Uacute;ltima avalia&ccedil;&atilde;o autom&aacute;tica de acessibilidade realizada no site: maio/2021.</p>
-            <ul>
-                <li>Avalia&ccedil;&atilde;o autom&aacute;tica utilizando o 
-                    <a class="link-primary" href="https://accessmonitor.acessibilidade.gov.pt/">
-                    avaliador autom&aacute;tico AccessMonitor</a> (WCAG 2.1): &Iacute;ndice 8.8.
-                </li>
-            </ul>
+            <div class="card bg-secondary px-5 py-4 mb-4">
+                <h3 class="mt-2 mb-4">Avalia&ccedil;&atilde;o de acessibilidade</h3>
+                <p>
+                    <b>&Uacute;ltima avalia&ccedil;&atilde;o autom&aacute;tica de acessibilidade realizada no site: maio/2021.</b>
+                </p>
+                <ul>
+                    <li>Avalia&ccedil;&atilde;o autom&aacute;tica utilizando o 
+                        <a class="link-primary" href="https://accessmonitor.acessibilidade.gov.pt/">
+                        avaliador autom&aacute;tico AccessMonitor</a> (WCAG 2.1): &Iacute;ndice 8.8.
+                    </li>
+                </ul>
+                
+                <p>
+                    <b>&Uacute;ltima avalia&ccedil;&atilde;o manual de acessibilidade realizada no site: --/2021.</b>
+                </p>
+                <ul>
+                    <li>Avalia&ccedil;&atilde;o manual, atrav&eacute;s de testes de usu&aacute;rios com baixa 
+                        vis&atilde;o e usu&aacute;rios cegos, utilizando leitores de tela NVDA e VoiceOver, e testes 
+                        realizados por desenvolvedores com experi&ecirc;ncia em acessibilidade na web.
+                    </li>
+                </ul>
+            </div>
             
-            <p>&Uacute;ltima avalia&ccedil;&atilde;o manual de acessibilidade realizada no site: --/2021.</p>
-            <ul>
-                <li>Avalia&ccedil;&atilde;o manual, atrav&eacute;s de testes de usu&aacute;rios com baixa 
-                    vis&atilde;o e usu&aacute;rios cegos, utilizando leitores de tela NVDA e VoiceOver, e testes 
-                    realizados por desenvolvedores com experi&ecirc;ncia em acessibilidade na web.
-                </li>
-            </ul>
 		</div>
 	</div>
 </div>
--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -2,7 +2,7 @@
 @section('titulo','RETACE - Sobre')
 @section('conteudo')
 
-<div class="container mt-5">
+<div class="container container-xl mt-5">
 	<?php 
         $data = [
             ['name' => 'RETACE', 'link' => url('/')],
@@ -14,9 +14,54 @@
             @include('layouts.breadcrumb', $data)
         </div>
     </div>
+    <hr class="mt-0">
 </div>
-
-<div class="container card my-3">
+<div id="" class="custom_content container custom-container card my-3 bg-transparent">
+	<div class="row">
+        <div class="col-12">
+            <div class="row">
+                <h2 class="h1 m-auto pt-4">Sobre o RETACE</h2>
+            </div>
+        </div>
+        
+		<div id="conteudo" class="col-12 pt-2 pl-2">
+			<div class="my-3">
+				<p style="text-align: center;">O RETACE é um repositório de Tecnologia Assistiva, vinculado ao contexto da educação, com o objetivo de permitir que os usuários encontrem possibilidades de tecnologia assistiva e todas as informações referentes a cada recurso em um só local.</p>
+                <br>
+                <h3>Buscando um recurso</h3>
+                <p>A busca pode ser realizada por termo ou por categoria. Ainda, através do menu, é possível acessar todos os recursos de Tecnologia Assistiva, bem como os materiais pedagógicos acessíveis cadastrados.</p>
+                <br>
+                <h3>Acessando um recurso</h3>
+                <p>Ao acessar um recurso, são disponibilizadas diversas informações, tais como: descrição do recurso, imagens e/ou vídeos relacionados; manuais de desenvolvimento (quando for o caso) e de uso, além de recursos relacionados. Também são disponibilizadas informações sobre fabricante/desenvolvedor, licença, número de visitas e avaliações.</p>
+                <img class="img-fluid rounded shadow-sm mt-4" src="{{url('/imagens/sobre_img1.jpg')}}" alt="imagem mostrando o menu com o mapa do site">
+                <div class="bg-white frase_destaque p-4 rounded-lg my-5">
+                    <p style="text-align: center;" class="m-0">O intuito do RETACE é permitir que os usuários<br> <b>encontrem possibilidades de tecnologia assistiva</b><br> e todas as informações referentes a cada recurso em um só local.</p>
+                </div>
+                <div class="row w-100 m-0" id="aprender_contribuir">
+                    <div class="d-flex flex-column justify-content-between bg-blue p-4 rounded-lg">
+                        <div>
+                            <h3 style="text-align: left;">Quer CONTRIBUIR com algum recurso?</h3>
+                            <p>A área CONTRIBUIR do repositório permite que qualquer pessoa possa incluir recursos de Tecnologia Assistiva ainda não cadastrados no repositório.</p>
+                        </div>
+                        <img class="img-fluid rounded mt-3" src="{{url('/imagens/sobre_img2.jpg')}}" alt="imagem mostrando o menu com o mapa do site">
+                    </div>
+                    <div class="d-flex flex-column justify-content-between bg-blue p-4 rounded-lg">
+                        <div>
+                            <h3>Quer APRENDER sobre Tecnologia Assistiva?</h3>
+                            <p>Na área APRENDER do repositório você encontra cursos, publicações e outros materiais que possibilitem aprender mais sobre Tecnologia Assistiva e sua aplicabilidade no contexto educacional.</p>
+                        </div>
+                        <img class="img-fluid rounded mt-3" src="{{url('/imagens/sobre_img3.jpg')}}" alt="imagem mostrando o menu com o mapa do site">
+                    </div>
+                </div>
+                <div style="max-width: 944px;" class="mx-auto mt-5 mb-5 historico">
+                    <h3>Histórico do RETACE</h3>
+                    <p>O RETACE foi desenvolvido e é gerenciado pelo Centro Tecnológico de Acessibilidade (CTA) do Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul (IFRS).<br><br>A criação do repositório teve início durante o Projeto Centro de Referência em Tecnologia Assistiva (CRTA), que teve financiamento da Secretaria de Educação Profissional e Tecnológica (SETEC) do Ministério da Educação (MEC), que teve como objetivo disseminar conhecimentos sobre o uso e o desenvolvimento de Tecnologia Assistiva de baixo custo e produção de materiais didático-pedagógicos acessíveis. Participaram do projeto, além do Centro Tecnológico de Acessibilidade - CTA (Reitoria) os campi do IFRS Bento Gonçalves, Caxias do Sul, Farroupilha, Porto Alegre, Restinga e Rio Grande.</p>
+                </div>
+			</div>		
+		</div>
+	</div>
+</div>
+<!--div class="container card my-3">
 	<div class="row">
         @if ($conteudoPagina->texto == '')
             <div class="col-12">
@@ -38,7 +83,7 @@
 			</div>		
 		</div>
 	</div>
-</div>
+</div-->
 @endsection
 @section('scripts')
 <script>
