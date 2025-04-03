b1a46a7295f378dc4c9c6793cca897b98120d853
Luana Chiomento <luanachiomento@gmail.com>
2025-04-03 09:07:48 -0300

    Atualização para destacar item do menu referente a página atual

--- resources/views/layouts/menuNavegacaoPrincipal.blade.php
+++ resources/views/layouts/menuNavegacaoPrincipal.blade.php
@@ -1,5 +1,5 @@
 <!-- Menu a ser exibido na tela principal, contendo imagem -->
-<nav class="menuTelaPrincipal navbar navbar-expand-lg navbar-light bg-sepia shadow-sm"
+<nav class="menuTelaPrincipal navbar navbar-expand-lg navbar-light shadow-sm"
 	aria-label="Menu de navegação principal">
 	<h1><a class="navbar-brand" href="{{ url('/') }}"><span class="h4">RETACE</span></a></h1>
 	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navegacaoPrincipal" aria-controls="navegacaoPrincipal" aria-expanded="false" aria-label="{{ __('Expandir menu') }}">
@@ -9,21 +9,21 @@
 	<a href="#" id="menu-principal" class="sr-only">Início do menu</a>
 	<div id="navegacaoPrincipal" class="collapse navbar-collapse">
 		<ul class="navbar-nav col-xs-9">
+			  <li class="nav-item">
+				<a class="nav-link {{ Request::is('filtro/tag/tecnologia assistiva') ? 'selected' : '' }}" href="{{ route('filtroTag', ['tecnologia assistiva']) }}">Tecnologia assistiva</a>
+			</li>
 			<li class="nav-item">
-        		<a class="nav-link dottedUnderline" href="{{route('filtroTag', ['tecnologia assistiva'])}}">Tecnologia assistiva</a>
-      		</li>
+				<a class="nav-link {{ Request::is('filtro/tag/material pedagógico') ? 'selected' : '' }}" href="{{ route('filtroTag', ['material pedagógico']) }}">Material pedagógico</a>
+			</li>
 			<li class="nav-item">
-        		<a class="nav-link dottedUnderline" href="{{route('filtroTag', ['material pedagógico'])}}">Material pedagógico</a>
-      		</li>
+				<a class="nav-link {{ Request::is('contribuir-ta') ? 'selected' : '' }}" href="{{ route('cadastrarTA') }}">Contribuir</a>
+			</li>
 			<li class="nav-item">
-        		<a class="nav-link dottedUnderline" href="{{route('cadastrarTA')}}">Contribuir</a>
-      		</li>
+				<a class="nav-link {{ Request::is('aprender') ? 'selected' : '' }}" href="{{ url('/aprender') }}">Aprender</a>
+			</li>
 			<li class="nav-item">
-        		<a class="nav-link dottedUnderline" href="{{ url('/aprender')}}">Aprender</a>
-      		</li>
-      		<li class="nav-item">
-        		<a class="nav-link dottedUnderline" href="{{ url('/sobre')}}">Sobre</a>
-      		</li>	  
+				<a class="nav-link {{ Request::is('sobre') ? 'selected' : '' }}" href="{{ url('/sobre') }}">Sobre</a>
+			</li>  
       	</ul>
 	</div>
 	<a href="#" class="sr-only">Final do menu</a>
