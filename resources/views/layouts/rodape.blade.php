6d5999ef6bba959d8d89d3995eacd9b41606554a
Luana Chiomento <luanachiomento@gmail.com>
2025-04-03 09:01:03 -0300

    Remoção CRTA do rodapé, melhoria na descrição dos links e ajuste das logos

--- resources/views/layouts/rodape.blade.php
+++ resources/views/layouts/rodape.blade.php
@@ -1,30 +1,39 @@
-<footer class="page-footer font-small bg-primary pt-4 mt-3">
+<footer class="page-footer font-small bg-primary mt-3">
 	<div class="container">
 		<!-- Grid row -->
-		<div class="row mb-4 d-flex flex-wrap align-items-center">
-			<!-- Grid column -->
-			<div class="col-md-8	 mt-3">
+		<div class="row d-flex flex-wrap align-items-center mt-4">
+			<div class="col-md-9">
 				<h2 class="h5">Entre em Contato</h2>
 				<p>
 					Para conhecer mais sobre os trabalhos e projetos desenvolvidos pelo CTA acesse nosso site ou redes sociais disponíveis na lista abaixo.
 				</p>
 				<ul class="list-unstyled">
-					<li class=""><a class="text-black-50 text-decoration-none" href="mailto:cta@ifrs.edu.br"><i class="bi bi-envelope"> </i>cta@ifrs.edu.br</a></li>
-					<li class=""><a class="text-black-50 text-decoration-none" href="https://cta.ifrs.edu.br/"><i class="bi bi-cursor"> </i>cta.ifrs.edu.br</a></li>
-					<li class=""><a class="text-black-50 text-decoration-none" href="https://instagram.com/cta-ifrs"><i class="bi bi-instagram"> </i>instagram.com/cta-ifrs</a></li>
-					<li class=""><a class="text-black-50 text-decoration-none" href="https://youtube.com/cta-ifrs"><i class="bi bi-youtube"> </i>youtube.com/cta-ifrs</a></li>
-					<li class=""><a class="text-black-50 text-decoration-none" href="https://facebook.com/cta-ifrs"><i class="bi bi-facebook"> </i>facebook.com/cta-ifrs</a></li>
+					<li class="">
+						<a class="text-black-50 text-decoration-none" href="mailto:cta@ifrs.edu.br"><i class="bi bi-envelope"> </i><span class="sr-only">E-mail do CTA</span>cta@ifrs.edu.br</a>
+					</li>
+					<li class="">
+						<a class="text-black-50 text-decoration-none" href="https://cta.ifrs.edu.br/"><i class="bi bi-cursor"> </i><span class="sr-only">Site do CTA</span>cta.ifrs.edu.br</a>
+					</li>
+					<li class="">
+						<a class="text-black-50 text-decoration-none" href="https://www.instagram.com/ctaifrs/"><i class="bi bi-instagram"> </i><span class="sr-only">Instagram do CTA</span> instagram.com/cta-ifrs</a>
+					</li>
+					<li class="">
+						<a class="text-black-50 text-decoration-none" href="https://youtube.com/cta-ifrs"><i class="bi bi-youtube"> </i><span class="sr-only">Canal no YouTube do CTA</span>youtube.com/cta-ifrs</a>
+					</li>
+					<li class="">
+						<a class="text-black-50 text-decoration-none" href="https://facebook.com/cta-ifrs"><i class="bi bi-facebook"> </i><span class="sr-only">Facebook do CTA</span>facebook.com/cta-ifrs</a>
+					</li>
 				</ul>
 			</div>
 
-			<div class="col-md-4 col-10 mt-4 pt-md-5 pt-2 logos">
+			<div class="bg-white col-md-3 col-10 pt-md-5 pt-2 logos">
 				<div class="row">
-					<div class="col-8">
-						<img class="w-100 border rounded" alt="Logo do CTA" src="{{url('/imagens/logo-cta-cor.png')}}">
+					<div class="col-12 p-0 pr-3">
+						<img class="w-100 rounded" alt="Logo do CTA" src="{{url('/imagens/logo-cta-cor.png')}}">
 					</div>
-					<div class="col-4">
-						<img class="w-100 border rounded mb-4" alt="Logo da PROEN" src="{{url('/imagens/logo-proen-cor.png')}}">
-						<img class="w-100 border rounded" alt="Logo do IFRS" src="{{url('/imagens/logo-ifrs-cor.png')}}">
+					<div class="col-12 p-0 pr-3 mt-3 logos-menores">
+						<img class="w-100 rounded mb-4" alt="Logo da PROEN" src="{{url('/imagens/logo-proen-cor.png')}}">
+						<img class="w-100 rounded" alt="Logo do IFRS" src="{{url('/imagens/logo-ifrs-cor.png')}}">
 					</div>
 				</div>
 			</div>
@@ -32,6 +41,6 @@
 		</div>
 	</div>
 	<div class="footer-copyright bg-secondary text-center py-3">
-		<span>CRTA | CTA | IFRS - {{date('Y')}}</span>
+		<span>CTA | IFRS - {{date('Y')}}</span>
 	</div>
 </footer>
